b5be5bb4c85fc07e9da9824cc1c34807bef1fb15
Asaad kouach <asaadk11@gmail.com>
2021-06-21 05:25:49 +0100

    Update fast_food.php

--- html/fast_food.php
+++ html/fast_food.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8" />
         <title>Mon repas-Fast Food</title>
         <link rel="stylesheet" href="../css/salade.css"/>
+        <link rel="stylesheet" href="../css/dropdown.css"/>
+        <script src="../js/dropdown.js"></script>
         <link rel="icon" type="image/png" sizes="18x18" href="../images/logo.png">
         <link href="../vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
        <script src="../vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
@@ -23,6 +25,25 @@ $conn = mysqli_connect($servername,$username,$password,$database);
                 src="../images/github.png" alt="Logo GitHub"></a>
                 <p id="titre" class="display-6"  >Mon repas</p>
             </div>
+            <div class="dropdown">
+                <?php
+                $nom = $_COOKIE['nom'];
+                echo "<button onclick='myFunction()' class='dropbtn' >$nom</button>";
+                ?>
+            <div id="myDropdown" class="dropdown-content">
+                    <a href="changer_mdp.php">Changer de mot de passe</a>
+                    <?php
+                        $sql1 = $conn->query("SELECT fonction FROM utilisateur WHERE nom_complet = '$nom' ") or die($conn->error);
+                        $fonction = $sql1->fetch_assoc();
+        if ($fonction['fonction']=='admin') 
+            {  echo'<a href="traitement_admins.php">Gestion admins</a>';
+              echo'<a href="traitement_recettes.php">Gestion recette </a>';
+              echo'<a href="traitement_ingrédients.php">Gestion ingrédient</a>';
+             }
+                     ?>
+                   <a href="login.php">Déconnexion</a>
+                </div>
+                </div>
         <h2 style="font-size:7vw"><b>Fast Food</b></h2>
 
         <div class="row row-cols-1 row-cols-md-3 g-4">
