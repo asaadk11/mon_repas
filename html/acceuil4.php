2b1c2126fa8dcb4ce050533169dd4d33ae26fd5e
Asaad kouach <asaadk11@gmail.com>
2021-06-21 05:13:10 +0100

    Update acceuil4.php

--- html/acceuil4.php
+++ html/acceuil4.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
+<head>
         <meta charset="utf-8" />
         <title>Mon repas</title>
-        <link rel="stylesheet" href="../css/acceuil4.css"/>
+        <link rel="stylesheet" href="../css/acceuil2.css"/>
+        <link rel="stylesheet" href="../css/dropdown.css"/>
+        <script src="../js/dropdown.js"></script>
         <link rel="icon" type="image/png" sizes="18x18" href="../images/logo.png">
         <link href="../vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
        <script src="../vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+       <?php
+$servername ='127.0.0.1';
+$database ='mon_repas';
+$username ='root';
+$password ='Neymar11';
+$conn = mysqli_connect($servername,$username,$password,$database);
+?>
         </head>
     <body>
     <div class="page-header">
@@ -16,6 +25,26 @@
                 src="../images/github.png" alt="Logo GitHub"></a>
                 <p id="titre" class="display-6"  >Mon repas</p>
             </div>
+            <div class="dropdown">
+                <?php
+                $nom = $_COOKIE['nom'];
+                echo "<button onclick='myFunction()' class='dropbtn' >$nom</button>";
+                ?>
+            <div id="myDropdown" class="dropdown-content">
+                    <a href="changer_mdp.php">Changer de mot de passe</a>
+                     <?php
+                        $sql1 = $conn->query("SELECT fonction FROM utilisateur WHERE nom_complet = '$nom' ") or die($conn->error);
+                        $fonction = $sql1->fetch_assoc();
+        if ($fonction['fonction']=='admin') 
+            {  echo'<a href="traitement_admins.php">Gestion admins</a>';
+              echo'<a href="traitement_recettes.php">Gestion recette </a>';
+              echo'<a href="traitement_ingrédients.php">Gestion ingrédient</a>';
+             }
+                     ?>
+                     <a href="login.php">Déconnexion</a>
+                </div>
+                </div>
+
             <img src="../images/dessert.jpg" class="salades_img" style="font-size:9vw">   
         <?php
        $cok = $_COOKIE['nom'];
